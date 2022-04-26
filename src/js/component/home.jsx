d416d528a0fbf38664a7fa4d7cea7ce050bfe079
YasZamora <yzamo001@gmail.com>
2022-04-26 01:15:15 +0000

    mi tarea 2

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,35 +7,35 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Lista = () => {
 	const [lista, setLista] = useState([
-		"Wash my hands",
-		"Make the bed",
-		"Wash my hands",
-		"Eat",
-		"Walk the dog",
+		{label:"Wash my hands", done: false},
+		{label:"Make the bed", done: false},
+		{label:"Wash my hands", done: false},
+		{label:"Eat", done: false},
+		{label:"Walk the dog", done:false},
 	]);
 	const FuncionsetLista = () => {
 		setLista([...lista, value]);
-		fetch("https://assets.breatheco.de/apis/fake/todos/user/alesanchezr", {
-			method: "PUT",
-			body: JSON.stringify(todos),
-			headers: {
-				"Content-Type": "application/json",
-			},
-		})
-			.then((resp) => {
-				console.log(resp.ok); // will be true if the response is successfull
-				console.log(resp.status); // the status code = 200 or code = 400 etc.
-				console.log(resp.text()); // will try return the exact result as string
-				return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
-			})
-			.then((data) => {
-				//here is were your code should start after the fetch finishes
-				console.log(data); //this will print on the console the exact object received from the server
-			})
-			.catch((error) => {
-				//error handling
-				console.log(error);
-			});
+		fetch('https://assets.breatheco.de/apis/fake/todos/user/yaszamora', {
+      method: "PUT",
+      body: JSON.stringify([...lista, value]),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(resp => {
+        console.log(resp.ok); // will be true if the response is successfull
+        console.log(resp.status); // the status code = 200 or code = 400 etc.
+        console.log(resp.text()); // will try return the exact result as string
+        return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+    })
+    .then(data => {
+        //here is were your code should start after the fetch finishes
+        console.log(data); //this will print on the console the exact object received from the server
+    })
+    .catch(error => {
+        //error handling
+        console.log(error);
+    });
 	};
 
 	const [value, setValue] = useState("");
