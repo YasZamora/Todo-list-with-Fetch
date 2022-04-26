487193065ddfbc71e519caa044eaee3c82f500af
YasZamora <yzamo001@gmail.com>
2022-04-26 02:17:29 +0000

    los otros fetch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ListGroup } from "react-bootstrap";
 
 //include images into your bundle
@@ -7,35 +7,50 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Lista = () => {
 	const [lista, setLista] = useState([
-		{label:"Wash my hands", done: false},
-		{label:"Make the bed", done: false},
-		{label:"Wash my hands", done: false},
-		{label:"Eat", done: false},
-		{label:"Walk the dog", done:false},
+		{ label: "Wash my hands", done: false },
+		{ label: "Make the bed", done: false },
+		{ label: "Wash my hands", done: false },
+		{ label: "Eat", done: false },
+		{ label: "Walk the dog", done: false },
 	]);
+	useEffect(() => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/yaszamora")
+			.then((resp) => {
+				return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+			})
+			.then((data) => {
+				//here is were your code should start after the fetch finishes
+				setLista(data);
+				console.log(data); //this will print on the console the exact object received from the server
+			})
+			.catch((error) => {
+				//error handling
+				console.log(error);
+			});
+	}, []);
 	const FuncionsetLista = () => {
 		setLista([...lista, value]);
-		fetch('https://assets.breatheco.de/apis/fake/todos/user/yaszamora', {
-      method: "PUT",
-      body: JSON.stringify([...lista, value]),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-    .then(resp => {
-        console.log(resp.ok); // will be true if the response is successfull
-        console.log(resp.status); // the status code = 200 or code = 400 etc.
-        console.log(resp.text()); // will try return the exact result as string
-        return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
-    })
-    .then(data => {
-        //here is were your code should start after the fetch finishes
-        console.log(data); //this will print on the console the exact object received from the server
-    })
-    .catch(error => {
-        //error handling
-        console.log(error);
-    });
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/yaszamora", {
+			method: "PUT",
+			body: JSON.stringify([...lista, value]),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		})
+			.then((resp) => {
+				console.log(resp.ok); // will be true if the response is successfull
+				console.log(resp.status); // the status code = 200 or code = 400 etc.
+				console.log(resp.text()); // will try return the exact result as string
+				return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+			})
+			.then((data) => {
+				//here is were your code should start after the fetch finishes
+				console.log(data); //this will print on the console the exact object received from the server
+			})
+			.catch((error) => {
+				//error handling
+				console.log(error);
+			});
 	};
 
 	const [value, setValue] = useState("");
@@ -51,6 +66,27 @@ const Lista = () => {
 		const listCopy = [...lista];
 		listCopy.splice(index, 1);
 		setLista(listCopy);
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/yaszamora", {
+			method: "PUT",
+			body: JSON.stringify(listCopy),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		})
+			.then((resp) => {
+				console.log(resp.ok); // will be true if the response is successfull
+				console.log(resp.status); // the status code = 200 or code = 400 etc.
+				console.log(resp.text()); // will try return the exact result as string
+				return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+			})
+			.then((data) => {
+				//here is were your code should start after the fetch finishes
+				console.log(data); //this will print on the console the exact object received from the server
+			})
+			.catch((error) => {
+				//error handling
+				console.log(error);
+			});
 	};
 	return (
 		<>
