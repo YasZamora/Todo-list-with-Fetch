bdf2338f789504c6498e146617979d3a51ce00cb
YasZamora <yzamo001@gmail.com>
2022-04-27 23:12:47 +0000

    finalizada

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { ListGroup } from "react-bootstrap";
 
 //include images into your bundle
@@ -7,11 +7,11 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Lista = () => {
 	const [lista, setLista] = useState([
-		{ label: "Wash my hands", done: false },
-		{ label: "Make the bed", done: false },
-		{ label: "Wash my hands", done: false },
-		{ label: "Eat", done: false },
-		{ label: "Walk the dog", done: false },
+		// { label: "Wash my hands", done: false },
+		// { label: "Make the bed", done: false },
+		// { label: "Wash my hands", done: false },
+		// { label: "Eat", done: false },
+		// { label: "Walk the dog", done: false },
 	]);
 	useEffect(() => {
 		fetch("https://assets.breatheco.de/apis/fake/todos/user/yaszamora")
@@ -29,18 +29,15 @@ const Lista = () => {
 			});
 	}, []);
 	const FuncionsetLista = () => {
-		setLista([...lista, value]);
+		setLista([...lista, { label: value, done: false }]);
 		fetch("https://assets.breatheco.de/apis/fake/todos/user/yaszamora", {
 			method: "PUT",
-			body: JSON.stringify([...lista, value]),
+			body: JSON.stringify([...lista, { label: value, done: false }]),
 			headers: {
 				"Content-Type": "application/json",
 			},
 		})
 			.then((resp) => {
-				console.log(resp.ok); // will be true if the response is successfull
-				console.log(resp.status); // the status code = 200 or code = 400 etc.
-				console.log(resp.text()); // will try return the exact result as string
 				return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
 			})
 			.then((data) => {
@@ -62,6 +59,31 @@ const Lista = () => {
 			FuncionsetLista();
 		}
 	};
+	const cleanAll = () => {
+		const newList = [];
+		setLista(newList);
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/yaszamora", {
+			method: "PUT",
+			body: JSON.stringify(newList),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		})
+			.then((resp) => {
+				console.log(resp.ok); // will be true if the response is successfull
+				console.log(resp.status); // the status code = 200 or code = 400 etc.
+				console.log(resp.text()); // will try return the exact result as string
+				return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+			})
+			.then((data) => {
+				//here is were your code should start after the fetch finishes
+				console.log(data); //this will print on the console the exact object received from the server
+			})
+			.catch((error) => {
+				//error handling
+				console.log(error);
+			});
+	};
 	const deleteItem = (index) => {
 		const listCopy = [...lista];
 		listCopy.splice(index, 1);
@@ -74,9 +96,6 @@ const Lista = () => {
 			},
 		})
 			.then((resp) => {
-				console.log(resp.ok); // will be true if the response is successfull
-				console.log(resp.status); // the status code = 200 or code = 400 etc.
-				console.log(resp.text()); // will try return the exact result as string
 				return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
 			})
 			.then((data) => {
@@ -99,7 +118,7 @@ const Lista = () => {
 					return (
 						<>
 							<ListGroup.Item className="Item">
-								<span>{elemento}</span>
+								<span>{elemento.label}</span>
 								<span
 									className="x"
 									onClick={() => deleteItem(index)}>
@@ -111,6 +130,17 @@ const Lista = () => {
 				})}
 			</ListGroup>
 			<div className="contador">{lista.length} item left</div>
+			<button
+				style={{
+					float: "right",
+					borderColor: "#DEC2C6",
+					boxShadow: "none",
+					color: "#DEC2C6",
+				}}
+				onClick={cleanAll}>
+				{" "}
+				Clean all
+			</button>
 		</>
 	);
 };
